8e9825c5a4247cc15df76ee5c816d5343d34edd2
Jayson Grace <jayson.e.grace@gmail.com>
2015-11-12 23:22:02 -0700

    Added helper scripts for development

---
+++ speech_app/scripts/generateTables.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# generateTables.sh
+#
+# Generate tables for speech application
+#
+# Usage: bash generateTables.sh
+#
+# Jayson Grace, jayson.e.grace@gmail.com, 11/12/2015
+#
+# Last update 11/12/2015 by Jayson Grace, jayson.e.grace@gmail.com
+# ----------------------------------------------------------------------------
+
+# Generate speaker scaffold
+rails g scaffold speaker name:string city:references state:references country:references native_language:references other_languages:integer age:integer english_onset:integer learning_method:integer english_country_residence:references length_english_residence:integer user:references
+
+# Generate city scaffold for dropdown
+rails g scaffold city name:string
+# Generate state scaffold for dropdown
+rails g scaffold state name:string
+# Generate country scaffold for dropdown
+rails g scaffold country name:string
+# Generate native language scaffold for dropdown
+rails g scaffold native_language name:string
+# Generate english country residence scaffold for dropdown
+rails g scaffold english_country_residence name:string
